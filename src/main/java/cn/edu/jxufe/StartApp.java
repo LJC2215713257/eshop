12b72c4a8aff0503f69657a4f46b971dde570e4b
ljc <774605118@qq.com>
2018-08-07 08:58:39 +0800

    初始项目+1

--- src/main/java/cn/edu/jxufe/StartApp.java
+++ src/main/java/cn/edu/jxufe/StartApp.java
@@ -10,5 +10,6 @@ public class StartApp {
     public static void main(String[] args) {
         System.out.println("网上商城服务正在启动！！！");
         SpringApplication.run(StartApp.class);
+        System.out.println("商城服务启动成功！！！");
     }
 }
