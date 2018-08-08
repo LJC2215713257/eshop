2620954125204a2ce8767d67aa3e6db3381a0a73
JL <1239831030@qq.com>
2018-08-08 17:38:24 +0800

    蒋仑

--- src/main/java/cn/edu/jxufe/dao/MemberinfoDAO.java
+++ src/main/java/cn/edu/jxufe/dao/MemberinfoDAO.java
@@ -3,9 +3,14 @@ package cn.edu.jxufe.dao;
 import cn.edu.jxufe.entity.Memberinfo;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * MemberinfoDAO继承基类
  */
 @Repository
 public interface MemberinfoDAO extends MyBatisBaseDao<Memberinfo, Integer> {
+
+    //判断是否存在
+    public List<Memberinfo> findUser(String tel,String pwd);
 }
